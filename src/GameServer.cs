211d2c9bdb4e92a14d6eee3c9d1abb4f0b23a12b
Joshua Jarvis <joshuajarvis0711@gmail.com>
2025-05-06 17:14:03 -0500

    Added socket basics.

--- src/GameServer.cs
+++ src/GameServer.cs
@@ -8,10 +8,17 @@ public class GameServer(ComputationLoop computationLoop)
 {
     private readonly ComputationLoop computationLoop = computationLoop;
     private readonly TcpListener tcpListener = new(IPAddress.Parse("127.0.0.1"), 8080);
-    private readonly Dictionary<string, Socket> sockets = [];
 
     public void Start()
     {
+        tcpListener.Start();
 
+        while (true)
+        {
+            // Each time a new socket is opened we should spawn a new thread to listen for data from it.
+            Socket socket = tcpListener.AcceptSocket();
+            Console.WriteLine("Accepted");
+            socket.Disconnect(true);
+        }
     }
 }
