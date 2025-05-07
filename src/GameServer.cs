2f7f8d10f7e3a54bb85b4c9c7cded6a9a6f348dd
Joshua Jarvis <joshuajarvis0711@gmail.com>
2025-05-07 17:17:29 -0500

    Started adding support for websockets.

--- src/GameServer.cs
+++ src/GameServer.cs
@@ -8,6 +8,10 @@ public class GameServer(ComputationLoop computationLoop)
 {
     private readonly ComputationLoop computationLoop = computationLoop;
     private readonly TcpListener tcpListener = new(IPAddress.Parse("127.0.0.1"), 8080);
+    private readonly Dictionary<Guid, TcpClient> _sockets = [];
+    private readonly int _maxSockets = 10;
+    // See RFC 6455, section 4.2.2
+    private readonly string _magicOongaBoongaBoogaloo = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
     public void Start()
     {
@@ -15,10 +19,51 @@ public class GameServer(ComputationLoop computationLoop)
 
         while (true)
         {
-            // Each time a new socket is opened we should spawn a new thread to listen for data from it.
-            Socket socket = tcpListener.AcceptSocket();
-            Console.WriteLine("Accepted");
-            socket.Disconnect(true);
+            // Using the simple one client per thread model, limit active threads to some reasonable value
+            // so we don't overwhelm system resources.
+            TcpClient socket = tcpListener.AcceptTcpClient();
+            if (_sockets.Count <= _maxSockets)
+            {
+                Guid socketId = Guid.NewGuid();
+                _sockets.Add(socketId, socket);
+
+                Thread socketThread = new(() => SocketReadLoop(socket, socketId));
+                socketThread.Start();
+            }
+            else
+            {
+                socket.Close();
+            }
         }
+    }
+
+    private void SocketReadLoop(TcpClient socket, Guid socketId)
+    {
+        byte[] buffer = new byte[1000];
+        while (true)
+        {
+            try
+            {
+                if (!socket.Connected)
+                {
+                    socket.Close();
+                    socket.Dispose();
+                    _sockets.Remove(socketId);
+                    return;
+                }
+
+                int bufferCount = socket.GetStream().Read(buffer);
+                PrintByteArrayInAscii(buffer, bufferCount);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+
+    public static void PrintByteArrayInAscii(byte[] bytes, int byteCount)
+    {
+        Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, byteCount));
     }
 }
